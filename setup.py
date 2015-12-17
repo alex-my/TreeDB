3037edf383ad097b6774c55021580ff7a38c8826
alex-my <alex_my@126.com>
2015-12-17 19:51:37 +0800

    modify pypi setup.py, version=0.0.6

--- setup.py
+++ setup.py
@@ -1,11 +1,6 @@
 #!/usr/bin/env python
 # coding:utf8
-
-try:
-    from setuptools import setup
-except ImportError:
-    from distutils.core import setup
-
+from setuptools import setup, find_packages
 
 readme = open('README.rst').read()
 history = open('HISTORY.rst').read().replace('.. :changelog', '')
@@ -13,15 +8,17 @@ history = open('HISTORY.rst').read().replace('.. :changelog', '')
 
 setup(
     name='TreeDB',
-    version='0.0.2',
+    version='0.0.6',
     description='be easy to use mysql driver.',
     long_description=readme + '\n\n' + history,
     author='Alex',
     author_email='alex_my@126.com',
     url='git@github.com:alex-my/TreeDB.git',
-    packages=['TreeDB'],
+    packages=find_packages(),
     include_package_data=True,
-    install_requires=[],
+    install_requires=[
+        'mysql-connector-python>=2.0.4'
+    ],
     license='MIT',
     zip_safe=False,
     keywords='TreeDB',
